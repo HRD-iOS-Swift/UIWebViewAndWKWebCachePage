0ea84762f6104323ba9da8abe1e05e97ea67d9e7
Kokpheng <yinkokpheng@yahoo.com>
2017-05-15 01:24:08 +0700

    Initial Commit

---
+++ WKWebCacheDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKWebCacheDemo
+//
+//  Created by Kokpheng on 5/15/17.
+//  Copyright © 2017 Kokpheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
